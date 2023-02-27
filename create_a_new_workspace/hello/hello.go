c993d7c89b2d5e8583555fed4e9750068a99c5ed
anarchymonkey <aniketbatabyal1996@gmail.com>
2023-02-27 17:16:30 +0530

    Added a new create_a_new_workspace module to teach how to create workspaces

---
+++ create_a_new_workspace/hello/hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("Hello"))
+
+	fmt.Println("+++++++++++++++++++++++++++++")
+
+	fmt.Println(stringutil.ToUpperCase("aniket"))
+}
