8fb11414ee2391448ab23db0b91eab9eb3a19515
anarchymonkey <aniketbatabyal1996@gmail.com>
2023-02-28 16:15:19 +0530

    added a new type interface called Number and moved the union there

--- create_generics/utils/utils.go
+++ create_generics/utils/utils.go
@@ -1,5 +1,10 @@
 package utils
 
+// This is the union type moved to a new type
+type Number interface {
+	int | int64 | float64
+}
+
 func SumOfNIntegers(numbers map[string]int) int {
 	var sum int = 0
 	for _, num := range numbers {
@@ -19,7 +24,7 @@ func SumOfNFloats(numbers map[string]float64) float64 {
 	return sum
 }
 
-func SumOfNNumbers[K comparable, V int | float64](m map[K]V) V {
+func SumOfNNumbers[K comparable, V Number](m map[K]V) V {
 	var s V
 
 	for _, v := range m {
