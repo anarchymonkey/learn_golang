615ce70eae7bd2131fba3335d0a5b34df0f7c98b
anarchymonkey <aniketbatabyal1996@gmail.com>
2023-03-06 22:13:59 +0530

    Added the final concepts into play such as error_handling, making generic templates for consumption & closures

--- create_a_webserver/webserver/webserver.go
+++ create_a_webserver/webserver/webserver.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"regexp"
 
 	"batsy.com/wiki"
 )
+
+const PUBLIC_DIR = "./public"
+
+var templates = template.Must(template.ParseFiles(PUBLIC_DIR+"/template.html", PUBLIC_DIR+"/view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-z0-9]+)$")
 
 func handlerFunc(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "text/html")
@@ -17,18 +24,36 @@ func handlerFunc(w http.ResponseWriter, req *http.Request) {
 // remove duplicacy
 
 func renderTemplate(htmlFileLocation string, writer http.ResponseWriter, page *wiki.Page) {
-	respTemplate, err := template.ParseFiles(htmlFileLocation)
+	err := templates.ExecuteTemplate(writer, htmlFileLocation, page)
 
 	if err != nil {
-		log.Fatal("Something went wrong", err)
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
 	}
-
-	respTemplate.Execute(writer, page)
 }
 
-func viewHandler(w http.ResponseWriter, req *http.Request) {
-	title := req.URL.Path[len("/view/"):]
+func getTitle(writer http.ResponseWriter, req *http.Request) (string, error) {
+	matched := validPath.FindStringSubmatch(req.URL.Path)
 
+	if matched == nil {
+		http.NotFound(writer, req)
+		return "", errors.New("invalid page Title")
+	}
+	return matched[2], nil
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		matched := validPath.FindStringSubmatch(req.URL.Path)
+
+		if matched == nil {
+			http.NotFound(w, req)
+			return
+		}
+		fn(w, req, matched[2])
+	}
+}
+
+func viewHandler(w http.ResponseWriter, req *http.Request, title string) {
 	p, err := wiki.LoadPage(title)
 
 	if err != nil {
@@ -36,37 +61,37 @@ func viewHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	renderTemplate("./public/view.html", w, p)
+	renderTemplate("view.html", w, p)
 }
 
-func editHandler(w http.ResponseWriter, req *http.Request) {
-	title := req.URL.Path[len("/edit/"):]
-
+func editHandler(w http.ResponseWriter, req *http.Request, title string) {
 	page, err := wiki.LoadPage(title)
 
 	if err != nil {
 		page = &wiki.Page{Title: "Tempfile"}
 	}
 
-	renderTemplate("./public/template.html", w, page)
+	renderTemplate("template.html", w, page)
 }
 
-func saveHandler(w http.ResponseWriter, req *http.Request) {
-	title := req.URL.Path[len("/save/"):]
+func saveHandler(w http.ResponseWriter, req *http.Request, title string) {
 	body := req.FormValue("body")
 
 	page := &wiki.Page{Title: title, Body: []byte(body)}
-	page.Save()
+	err := page.Save()
 
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 	http.Redirect(w, req, "/view/"+title, http.StatusFound)
 }
 
 func main() {
 
 	http.HandleFunc("/", handlerFunc)
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
