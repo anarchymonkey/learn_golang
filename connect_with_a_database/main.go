feca16de31c395da6684bf04363a5ec9a635fbd4
anarchymonkey <aniketbatabyal1996@gmail.com>
2023-03-24 23:09:16 +0530

    Added crud operations with individual methods for insert, update and view row and view all rows

--- connect_with_a_database/main.go
+++ connect_with_a_database/main.go
@@ -15,13 +15,67 @@ type DummyTable struct {
 	Email string `json:"email"`
 }
 
-func connectHandler() {
+func printAllRowsInTable(conn *pgx.Conn) {
+	rows, err := conn.Query(context.Background(), "select * from dummy_table")
 
-	// env := os.Environ()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "There is a error while querying \n %v", err)
+		os.Exit(1)
+	}
 
-	// for _, val := range env {
-	// 	fmt.Println(val)
-	// }
+	defer rows.Close()
+
+	datas, _ := pgx.CollectRows(rows, pgx.RowToAddrOfStructByName[DummyTable])
+
+	for _, data := range datas {
+		fmt.Println("data is", data)
+	}
+}
+
+func printRowAfterInserting(conn *pgx.Conn) {
+	const QUERY = "INSERT INTO dummy_table(id, name, age, email) values($1,$2,$3,$4);"
+
+	row, err := conn.Exec(context.Background(), QUERY, 15, "Aniket", 27, "aniket6991@gmail.com")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error while inserting data into the table \n %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(row.RowsAffected())
+	fmt.Println("Row after inserting")
+	printRowInTable(conn, 14)
+}
+
+func printRowAfterUpdating(conn *pgx.Conn) {
+	const QUERY = "UPDATE dummy_table set name=$1 WHERE id=$2;"
+
+	row, err := conn.Exec(context.Background(), QUERY, "Ankush", 15)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error while update data on the table \n %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Rows affected", row.RowsAffected())
+	fmt.Println("Row after updating")
+	printRowInTable(conn, 14)
+}
+
+func printRowInTable(conn *pgx.Conn, id int) {
+
+	var dummy DummyTable
+	err := conn.QueryRow(context.Background(), "SELECT * from dummy_table where id=$1", id).Scan(&dummy.Id, &dummy.Name, &dummy.Age, &dummy.Email)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error occoured while scanning row \n %v", err)
+		os.Exit(1)
+	}
+	fmt.Println("Dummy table row is", dummy)
+}
+
+func getDBConnectionHandler() *pgx.Conn {
+
 	dbUrl := fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", "localhost", 5432, os.Getenv("PG_USERNAME_V1"), os.Getenv("PG_PASS_V1"), "postgres",
 	)
@@ -35,37 +89,24 @@ func connectHandler() {
 		os.Exit(1)
 	}
 
-	defer connection.Close(context.Background())
-
-	// rows, err := connection.Query(context.Background(), "select * from dummy_table;")
-
-	// err = pgxscan.Select(context.Background(), connection, &rows, "select * from dummy_table;")
-
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "The error is \n %v", err)
-	// 	os.Exit(1)
-	// }
-
-	rows, err := connection.Query(context.Background(), "select * from dummy_table")
-
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "There is a error while querying \n %v", err)
-		os.Exit(1)
-	}
-
-	defer rows.Close()
-
-	datas, _ := pgx.CollectRows(rows, pgx.RowToAddrOfStructByName[DummyTable])
-
-	for _, data := range datas {
-		fmt.Println("data is", data.Name)
-	}
+	return connection
 }
 
 func main() {
 
-	connectHandler()
+	conn := getDBConnectionHandler()
+	defer conn.Close(context.Background())
 
-	fmt.Println("Here is a go file")
+	fmt.Println("Getting all rows in dummy table:")
+	printAllRowsInTable(conn)
+
+	fmt.Println("\nGetting one row in dummy table:")
+	printRowInTable(conn, 8)
+
+	fmt.Println(("\nInserting one row in dummy table"))
+	printRowAfterInserting(conn)
+
+	fmt.Println(("\nUpdating one row in dummy table"))
+	printRowAfterUpdating(conn)
 
 }
